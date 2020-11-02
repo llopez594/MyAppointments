27a59a12539943727f37c9ebf2d59dcdebcd1b63
llopezm <Luisjose20>
2020-11-02 19:24:53 -0400

    primeras 3 vistas del proyecto

--- app/src/main/java/com/ljlopezm/myappointments/MainActivity.kt
+++ app/src/main/java/com/ljlopezm/myappointments/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.ljlopezm.myappointments
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+
+        tvGoToRegister.setOnClickListener {
+            Toast.makeText(this, getString(R.string.please_fill_your_register_data), Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
